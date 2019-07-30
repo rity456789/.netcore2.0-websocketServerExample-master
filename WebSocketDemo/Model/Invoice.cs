8ec18aa6365cea4ee9fab9a83da4261256cc30cd
phbinh <rity456789@gmail.com>
2019-07-30 11:13:41 +0700

    fix bug push noti

--- WebSocketDemo/Model/Invoice.cs
+++ WebSocketDemo/Model/Invoice.cs
@@ -12,11 +12,12 @@ namespace WebSocketDemo.Model
         public string Message { get; set; }
         public int SenderId { get; set; }
         public int ReceiverId { get; set; }
+        public int Method { get; set; }
         public int Type { get; set; }
         public string Time { get; set; }
         public int Status { get; set; }
         public bool ReplyInvoice { get; set; }
-        public int ReplyID { get; set; }
+        public int ReplyId { get; set; }
 
         public Invoice(JObject data)
         {
@@ -24,11 +25,17 @@ namespace WebSocketDemo.Model
             Message = data["Message"].ToString();
             SenderId = Int32.Parse(data["SenderId"].ToString());
             ReceiverId = Int32.Parse(data["ReceiverId"].ToString());
+            Method = Int32.Parse(data["Method"].ToString());
             Type = Int32.Parse(data["Type"].ToString());
             Time = data["Time"].ToString();
             Status = Int32.Parse(data["Status"].ToString());
             ReplyInvoice = bool.Parse(data["ReplyInvoice"].ToString());
-            ReplyID = Int32.Parse(data["ReplyID"].ToString());
+            ReplyId = Int32.Parse(data["ReplyId"].ToString());
+        }
+
+        public Invoice()
+        {
+
         }
     }
 }
--- WebSocketDemo/WebSockets/ChatRoomHandler.cs
+++ WebSocketDemo/WebSockets/ChatRoomHandler.cs
@@ -13,7 +13,9 @@ namespace WebSocketDemo.WebSockets
 {
     public class ChatRoomHandler : WebSocketHandler
     {
-        static HttpClient client = new HttpClient(); //read db from api
+        private static readonly HttpClient Client = new HttpClient(); //read db from api
+        private const string UrlString = "https://localhost:44373/api/invoice";
+
         public ChatRoomHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
         {
 
@@ -23,33 +25,64 @@ namespace WebSocketDemo.WebSockets
             string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
             if (message.Substring(0, 4) == "1a2b")
             {
-                var sId = WebSocketConnectionManager.GetId(socket);
-                var uId = Int32.Parse(message.Substring(5));
-                WebSocketConnectionManager.SetUserIdentification(sId, uId);
+                Identify(socket, message);
             }
             else
             {
                 JObject data = JObject.Parse(message);
                 Invoice invoice = new Invoice(data);
-                var urlString = "https://localhost:44373/api/invoice";
-
-                var receiver = invoice.ReceiverId;
-                var receiverWSID = WebSocketConnectionManager.GetUserIdentification(receiver);
-                if (receiverWSID == "") //receiver doesn't connect to socket
-                {   //store invoice to db without change status
-                    var uri = new Uri(urlString);
-                    var jsonInString = JsonConvert.SerializeObject(invoice);
-                    var response = await client.PostAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
+                if (!invoice.ReplyInvoice)
+                {   //reply task
+                    await StoreAndSendInvoice(invoice);
                 }
                 else
-                {
-                    invoice.Status = 1; //sent
-                    var uri = new Uri(urlString);
-                    var jsonInString = JsonConvert.SerializeObject(invoice);
-                    var response = await client.PostAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
-                    await SendMessageAsync(receiverWSID, message);
-                }               
+                {   //reply other invoice
+                    //update status (3-reply) of invoice replied by this invoice
+                    await UpdateInvoiceStatus(invoice);
+                }
             }
+        }
+
+        private async Task UpdateInvoiceStatus(Invoice invoice)
+        {
+            invoice.Status = 3; //approved
+            var uri = new Uri(UrlString + "/" + invoice.ReplyId.ToString());
+            var jsonInString = JsonConvert.SerializeObject(invoice);
+            var response = await Client.PutAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
+        }
+
+        private async Task StoreAndSendInvoice(Invoice invoice)
+        {
+            var receiver = invoice.ReceiverId;
+            var receiverWsid = WebSocketConnectionManager.GetUserIdentification(receiver);
+            var message = "";
+            if (receiverWsid == "") //receiver doesn't connect to socket
+            {   //store invoice to db without change status
+                var uri = new Uri(UrlString);
+                var jsonInString = JsonConvert.SerializeObject(invoice);
+                var response = await Client.PostAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
+            }
+            else
+            {
+                invoice.Status = 1; //sent
+                var uri = new Uri(UrlString);
+                var jsonInString = JsonConvert.SerializeObject(invoice);
+                var response = await Client.PostAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
+                if (response.IsSuccessStatusCode)
+                {
+                    var rs = await response.Content.ReadAsAsync<Invoice>();
+                    jsonInString = JsonConvert.SerializeObject(rs);
+                    message = jsonInString;
+                }
+                await SendMessageAsync(receiverWsid, message);
+            }
+        }
+
+        private void Identify(WebSocket socket, string message)
+        {
+            var sId = WebSocketConnectionManager.GetId(socket);
+            var uId = Int32.Parse(message.Substring(5));
+            WebSocketConnectionManager.SetUserIdentification(sId, uId);
         }
     }
 }
